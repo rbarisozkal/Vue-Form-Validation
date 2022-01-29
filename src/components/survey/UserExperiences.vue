18bc064a5ae8b8b1e6f6e444842afb246b1d884f
Renas Barış Özkal <barisozkal11@gmail.com>
2022-01-30 00:50:52 +0300

    connected to database

---
+++ src/components/survey/UserExperiences.vue
@@ -0,0 +1,68 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch(`https://vueproject-5a032-default-rtdb.firebaseio.com/surveys.json`)
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].userName,
+              rating: data[id].rating,
+            });
+          }
+          this.results=results;
+        });
+    },
+   
+  },
+  created(){
+    this.loadExperiences();
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
